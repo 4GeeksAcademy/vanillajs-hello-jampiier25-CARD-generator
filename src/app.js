8cc1f2aa0d95dbe38b538287fd73c97f9b380ed5
Jean Pierre Pluas <jampiier@gmail.com>
2025-02-12 22:09:04 +0000

    jampiier25

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   generarCarta();
+  
 }
 function generarCarta() {
-  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
   let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
   let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
   let randomSuitNumber = Math.floor(Math.random() * suit.length);
@@ -20,6 +21,7 @@ function generarCarta() {
   document.getElementById('theCard').classList.add("card");
   document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
 }
+let intervalo;
 
 function getSuiteClass(suit) {
   switch (suit) {
@@ -29,3 +31,26 @@ function getSuiteClass(suit) {
     case "Clubs": return "suit-clubs";
   }
 }
+document.getElementById("generateCard").addEventListener('click', generarCarta);
+
+//setInterval(generarCarta, 10000); intervalo para hacerlo en 10s sin necesidad de un botón//
+
+function iniciarTemporizador() {
+  if (!intervalo) {
+    intervalo = setInterval(generarCarta, 10000); //intervalo de 10s para usarlo con un botón//
+  }
+}
+
+function detenerTemporizador() {
+  clearInterval(intervalo);
+  intervalo = null;
+} // funcion para detener el temporizador//
+
+//boton inicio temp.//
+document.getElementById('startButton').addEventListener('click', iniciarTemporizador);
+
+// boton stop temp.//
+document.getElementById('stopButton').addEventListener('click', detenerTemporizador);
+
+
+document.getElementById('cardContent').style.color = '';
