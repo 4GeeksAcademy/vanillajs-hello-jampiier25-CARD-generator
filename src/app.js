2769ecc426ec6969a98c7d3c1c27952cc5ddbe7b
Jean Pierre Pluas <jampiier@gmail.com>
2025-02-16 18:06:18 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -1,78 +1,74 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
   generarCarta();
   
-
+//funcion para generar una carta aleatoria//
 function generarCarta() {
-  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
-  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
-  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
-  let randomSuitNumber = Math.floor(Math.random() * suit.length);
-  let finalSuit = suit[randomSuitNumber];
-  
-  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
-  document.getElementById('theCard').className = "";
-  document.getElementById('theCard').classList.add("card");
-  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+    let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
+    let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+    let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+    let randomSuitNumber = Math.floor(Math.random() * suit.length);
+    let finalSuit = suit[randomSuitNumber];
+    let finalCardNumber = cardNumber[randomCardNumber];
+    
+    document.getElementById('cardContent').innerHTML = finalCardNumber;
+    document.getElementById('theCard').className = "";
+    document.getElementById('theCard').classList.add("card");
+    document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
 
-  document.getElementById('ancho').addEventListener('input', e =>{
-    document.getElementById('theCard').style.width= e.target.value + 'px'
-  })
-  
-  document.getElementById('alto').addEventListener('input', e =>{
-    document.getElementById('theCard').style.height= e.target.value + 'px'
-  })
-}
-let intervalo;
+    //modficar el tamaño de la carta por parte del usuario // 
+    //input ancho//
+    document.getElementById('ancho').addEventListener('input', e =>{
+      document.getElementById('theCard').style.width= e.target.value + 'px'
+    })
+    //input alto//
+    document.getElementById('alto').addEventListener('input', e =>{
+      document.getElementById('theCard').style.height= e.target.value + 'px'
+    })
+    //boton inicio temp.//
+    document.getElementById('startButton').addEventListener('click', iniciarTemporizador);
+    // boton stop temp.//
+    document.getElementById('stopButton').addEventListener('click', detenerTemporizador);
+    //boton generar carta//
+    document.getElementById("generateCard").addEventListener('click', generarCarta);
+  //obtener el valor de la carta con su respectivo color de palo//
+    document.getElementById('cardContent').style.color = getColorBySuit(finalSuit);
 
-function getSuiteClass(suit) {
-  switch (suit) {
-    case "Diamonds": return "suit-diamonds";
-    case "Spades": return "suit-spades";
-    case "Hearts": return "suit-hearts";
-    case "Clubs": return "suit-clubs";
+  //funcion para obtener el palo//
+  function getSuiteClass(suit) {
+    switch (suit) {
+      case "Diamonds": return "suit-diamonds";
+      case "Spades": return "suit-spades";
+      case "Hearts": return "suit-hearts";
+      case "Clubs": return "suit-clubs";
+    }
   }
-}
-document.getElementById("generateCard").addEventListener('click', generarCarta);
-
-//setInterval(generarCarta, 10000); intervalo para hacerlo en 10s sin necesidad de un botón//
-
-function iniciarTemporizador() {
-  if (!intervalo) {
-    intervalo = setInterval(generarCarta, 5000); //intervalo de 5s para usarlo con un botón//
+  //funcion para definir el valor de la carta segun el color del palo //
+  function getColorBySuit(suit) {
+    switch (suit) {
+      case "Diamonds": return "red";  
+      case "Hearts": return "red";    
+      case "Spades": return "black";  
+      case "Clubs": return "black";   
+      default: return "black"; 
+    }
   }
+  //setInterval(generarCarta, 10000); intervalo para hacerlo en 10s sin necesidad de un botón para poder usarlo en un futuro//
+  //variable y funcion para los botones de empezar y detener el temporizador//
+  let intervalo;
+  //empezar temporizador//
+  function iniciarTemporizador() {
+    if (!intervalo) {
+      intervalo = setInterval(generarCarta, 5000); //intervalo de 5s para usarlo con un botón no le puse 10s ya que me resultaba demasiado tiempo para poder notar el temporizador//
+    }
+  }
+  //detener temporizador//
+  function detenerTemporizador() {
+    clearInterval(intervalo);
+    intervalo = null;
+  }
+   
 }
-
-function detenerTemporizador() {
-  clearInterval(intervalo);
-  intervalo = null;
-} // funcion para detener el temporizador//
-
-//boton inicio temp.//
-document.getElementById('startButton').addEventListener('click', iniciarTemporizador);
-
-// boton stop temp.//
-document.getElementById('stopButton').addEventListener('click', detenerTemporizador);
-
-
-document.getElementById('cardContent').style.color = ' generarCarta ';
-
-
-//   miInput.addEventListener("input", e => {
-
-//})//
-
-//agregar un label input para modificar el width y height de la carta//
-
-
-
-
-
-
 }
